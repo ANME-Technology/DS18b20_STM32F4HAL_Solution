b74a5261f401afbd4cfd461f7d89c4b4e4e6b8be
MCqie <1051846107@qq.com>
2021-04-21 13:40:16 +0800

    First

---
+++ cmake-build-debug/CMakeFiles/D2.elf.dir/DependInfo.cmake
@@ -0,0 +1,71 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/STM32Env/ClionSTM32Proj/D2/startup/startup_stm32f407xx.s" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/startup/startup_stm32f407xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/STM32Env/ClionSTM32Proj/D2/Core/Src/gpio.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Core/Src/gpio.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Core/Src/main.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Core/Src/main.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Core/Src/stm32f4xx_hal_msp.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Core/Src/stm32f4xx_it.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Core/Src/syscalls.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Core/Src/syscalls.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Core/Src/system_stm32f4xx.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "D:/STM32Env/ClionSTM32Proj/D2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "D:/STM32Env/ClionSTM32Proj/D2/cmake-build-debug/CMakeFiles/D2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
